9eec5c288a44b6c4b4cc6a7a53dfb7dec30106ae
Jeremy Watt <jermwatt@gmail.com>
2024-09-27 08:16:07 -0700

    dropdown

--- js/dropdown.js
+++ js/dropdown.js
@@ -4,13 +4,38 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuButtonHidden = document.querySelector("#menu-button-hidden");
     const menuButtonOpen = document.querySelector("#menu-button-open");
     const mobileMenu = document.querySelector("#mobile-menu");
+    let ignoreFirstOutsideClick = true;
 
-    function showMenu() {
-      console.log("hamburger menu clicked");
+    function toggler() {
       menuButtonOpen.classList.toggle("hidden");
       menuButtonHidden.classList.toggle("hidden");
       mobileMenu.classList.toggle("hidden");
     }
-    mobileMenuButton.addEventListener("click", showMenu);
+
+    function mobileMenuActions(e) {
+      if (!ignoreFirstOutsideClick) {
+        if (!mobileMenu.classList.contains("hidden")) {
+          if (!mobileMenu.contains(e.target)) {
+            console.log("clicking outside of menu");
+            toggler();
+          } else {
+            console.log("clicking in menu");
+          }
+        }
+      }
+      ignoreFirstOutsideClick = false;
+    }
+
+    function toggleMobileMenu(e) {
+      console.log("hamburger menu clicked");
+      toggler();
+      if (!mobileMenu.classList.contains("hidden")) {
+        ignoreFirstOutsideClick = true;
+        document.addEventListener("click", mobileMenuActions);
+      } else {
+        document.removeEventListener("click", mobileMenuActions);
+      }
+    }
+    mobileMenuButton.addEventListener("click", toggleMobileMenu);
   }, 500);
 });
