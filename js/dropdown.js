3ecc6583ce93a845c100f2c432941c7e6e81db09
Jeremy Watt <jermwatt@gmail.com>
2024-09-27 07:48:03 -0700

    dropdown work

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,14 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    const mobileMenu = document.querySelector("#menu-button");
+    const mobileMenuButton = document.querySelector("#menu-button");
     const menuButtonHidden = document.querySelector("#menu-button-hidden");
     const menuButtonOpen = document.querySelector("#menu-button-open");
+    const mobileMenu = document.querySelector("#mobile-menu");
 
     function showMenu() {
       console.log("hamburger menu clicked");
       menuButtonOpen.classList.toggle("hidden");
       menuButtonHidden.classList.toggle("hidden");
+      mobileMenu.classList.toggle("hidden");
     }
-    mobileMenu.addEventListener("click", showMenu);
+    mobileMenuButton.addEventListener("click", showMenu);
   }, 500);
 });
