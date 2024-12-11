16298a4e8541b792786066762509c165548216bb
Jeremy Watt <jermwatt@gmail.com>
2024-12-11 10:18:37 -0700

    refactor for tailwind cli

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js}", // Update this with the path to your files
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
